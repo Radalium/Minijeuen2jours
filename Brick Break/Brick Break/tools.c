bb0d7536c672e710f07730b294cd62fe41b6bbff
Valette Evan <valetteevan23@gmail.com>
2023-12-18 12:58:17 +0100

    Add

--- Brick Break/Brick Break/tools.c
+++ Brick Break/Brick Break/tools.c
@@ -1,2 +1,26 @@
 #include "tools.h"
+#include "time.h"
+#include "SFML/Graphics.h"
 
+sfTime sftime;
+sfClock* sfclock;
+
+void initTools()
+{
+    sfclock = sfClock_create();
+    srand(time(NULL));
+}
+
+
+float getDeltaTime()
+{
+    if (sfTime_asSeconds(sftime) > 1.0f)
+        return 1.0f;
+    else
+        return sfTime_asSeconds(sftime);
+}
+
+void restartClock()
+{
+    sftime = sfClock_restart(sfclock);
+}
--- Brick Break/Brick Break/tools.h
+++ Brick Break/Brick Break/tools.h
@@ -2,6 +2,10 @@
 #include "tools.c"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "SFML/Graphics.h"
 
- 
+
+void initTools();
+float getDeltaTime();
+void restartClock();
